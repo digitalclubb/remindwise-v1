6b73294eb1d718219862a7e5ec204d0a97198dd7
digitalclubb <thewelsh@gmail.com>
2023-05-16 13:03:51 +0100

    chore: add cat

--- apps/app/src/components/navigation/Navigation.svelte
+++ apps/app/src/components/navigation/Navigation.svelte
@@ -16,6 +16,12 @@
         }
         `,
     });
+
+    let showForm = false;
+
+    const addCategory = () => {
+        showForm = !showForm;
+    };
 </script>
 
 <nav>
@@ -23,30 +29,35 @@
         <h2>Gareth Clubb</h2>
         <p>someemail@domain.com</p>
     </div>
-    <ul>
-        <li>Money</li>
-        <li>Insurance</li>
-        <li>Entertainment</li>
-        <li>Health</li>
-        <li>Travel</li>
-        <li>Transport</li>
-        <li>Utilities</li>
-        <li>Misc</li>
-    </ul>
+
+    {#if $categories.fetching}
+        <p>Loading...</p>
+    {:else if $categories.error}
+        <p>Oh no... {$categories.error.message}</p>
+    {:else}
+        <ul>
+            {#each $categories.data.categoriesCollection.edges as category}
+                <li>{category.node.name}</li>
+            {/each}
+            {#if showForm}
+            <li>
+                <form>
+                    <input type="text" name="category" required />
+                    <button type="submit">Add</button>
+                </form>
+            </li>
+            {/if}
+            <li>
+                <button on:click={addCategory}>Add category</button>
+            </li>
+        </ul>
+    {/if}
     <ul>
         <li>Help</li>
         <li>Settings</li>
     </ul>
 </nav>
 
-{#if $categories.fetching}
-<p>Loading...</p>
-{:else if $categories.error}
-<p>Oh no... {$categories.error.message}</p>
-{:else}
-<ul>
-  {#each $categories.data.categoriesCollection.edges as category}
-  <li>{category.node.name}</li>
-  {/each}
-</ul>
-{/if}
+<style>
+    li {text-transform: capitalize;}
+</style>
