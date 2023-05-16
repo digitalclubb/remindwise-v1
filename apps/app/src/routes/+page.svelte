fd9d155f611056de29c941bc19f93ed5fb6346b5
aferrugento <adrianaferrugento@gmail.com>
2023-05-16 11:59:17 +0100

    chore: delete log

--- apps/app/src/routes/+page.svelte
+++ apps/app/src/routes/+page.svelte
@@ -1,30 +1,31 @@
 <script lang="ts">
-  import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY} from '$env/static/public';
+  import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY } from "$env/static/public";
 
-  import { createClient, setContextClient, cacheExchange, fetchExchange  } from '@urql/svelte'
+  import {
+    createClient,
+    setContextClient,
+    cacheExchange,
+    fetchExchange
+  } from "@urql/svelte";
 
   const headers = {
     apikey: PUBLIC_SUPABASE_KEY,
-    authorization: `Bearer ${PUBLIC_SUPABASE_KEY}`,
-  }
-
-  console.log(PUBLIC_SUPABASE_URL)
+    authorization: `Bearer ${PUBLIC_SUPABASE_KEY}`
+  };
 
   const client = createClient({
     url: PUBLIC_SUPABASE_URL,
     exchanges: [cacheExchange, fetchExchange],
     fetchOptions: function createFetchOptions() {
-      return { headers }
-    },
+      return { headers };
+    }
   });
 
   setContextClient(client);
 
-  import Navigation from '../components/navigation/Navigation.svelte';
+  import Navigation from "../components/navigation/Navigation.svelte";
 </script>
 
 <h1>Reminder App</h1>
 
 <Navigation />
-
-
