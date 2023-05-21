8d1af7737cd7b167be7e5df28c97a4fd8825d15a
digitalclubb <thewelsh@gmail.com>
2023-05-21 19:20:31 +0100

    chore: stacked chart

--- apps/app/src/routes/+page.svelte
+++ apps/app/src/routes/+page.svelte
@@ -1,10 +1,42 @@
 <script lang="ts">
-  import ChartBar from "../components/charts/bar/Bar.svelte";
+  import ChartBar from "../components/charts/bar/Index.svelte";
+  import ChartStacked from "../components/charts/stacked/Index.svelte";
 
-  const dummyData = [
+  const dummyBarData = [
     { category: "insurance", value: 2 },
     { category: "entertainment", value: 7 },
     { category: "something", value: 5 },
+  ];
+
+  const dummyStackedData = [
+    {
+      category: "insurance",
+      reminder1: 3840,
+      reminder2: 1920,
+      reminder3: 960,
+      reminder4: 400,
+    },
+    {
+      category: "entertainment",
+      reminder1: 1600,
+      reminder2: 1440,
+      reminder3: 960,
+      reminder4: 400,
+    },
+    {
+      category: "something",
+      reminder1: 820,
+      reminder2: 1000,
+      reminder3: 640,
+      reminder4: 400,
+    },
+    {
+      category: "else",
+      reminder1: 820,
+      reminder2: 560,
+      reminder3: 720,
+      reminder4: 400,
+    },
   ];
 
   /** @type {import('./$types').LayoutData} */
@@ -13,27 +45,56 @@
 
 <h1>Dashboard</h1>
 
-<h2>Most used categories</h2>
-<ChartBar data={dummyData} />
+<section>
+  <h2>Categories</h2>
 
-<h2>Upcoming reminders</h2>
-<table>
-  <thead>
-    <tr>
-      <th>Company</th>
-      <th>Category</th>
-      <th>Due date</th>
-      <th>Auto renewal?</th>
-    </tr>
-  </thead>
-  <tbody>
-    {#each data.reminders as reminders}
+  <div>
+    <section>
+      <h3>Most used</h3>
+      <ChartBar data={dummyBarData} />
+    </section>
+
+    <section>
+      <h3>Outgoings</h3>
+      <ChartStacked data={dummyStackedData} />
+    </section>
+  </div>
+</section>
+
+<section>
+  <h2>Upcoming reminders</h2>
+  <table>
+    <thead>
       <tr>
-        <td>{reminders.reminder.company}</td>
-        <td>{reminders.reminder.categoryId}</td>
-        <td>{reminders.reminder.dateOfRenewal}</td>
-        <td>{reminders.reminder.autoRenewal}</td>
+        <th>Company</th>
+        <th>Category</th>
+        <th>Due date</th>
+        <th>Auto renewal?</th>
+        <th>Telephone</th>
+        <th>Email</th>
       </tr>
-    {/each}
-  </tbody>
-</table>
+    </thead>
+    <tbody>
+      {#each data.reminders as reminders}
+        <tr>
+          <td>{reminders.reminder.company}</td>
+          <td>{reminders.reminder.categoryId}</td>
+          <td>{reminders.reminder.dateOfRenewal}</td>
+          <td>{reminders.reminder.autoRenewal}</td>
+          <td />
+          <td />
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+</section>
+
+<style>
+  div {
+    display: grid;
+    padding-left: 4rem;
+    padding-right: 4rem;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 5rem;
+  }
+</style>
