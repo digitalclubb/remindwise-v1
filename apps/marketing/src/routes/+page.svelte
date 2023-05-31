0f8d3bc297f58363883765d2dd4e2c62b2b7f862
digitalclubb <thewelsh@gmail.com>
2023-05-31 18:19:59 +0100

    chore: marketing temp

--- apps/marketing/src/routes/+page.svelte
+++ apps/marketing/src/routes/+page.svelte
@@ -1,10 +1,31 @@
-<script lang="ts">
-	import { Button } from 'components';
-</script>
+<header>
+	<a href="">LOGO</a>
+	<nav>
+		<ul>
+			<li>
+				<a href="">About</a>
+			</li>
+			<li>
+				<a href="">Pricing</a>
+			</li>
+			<li>
+				<a href="">Get BRAND</a>
+			</li>
+		</ul>
+	</nav>
+</header>
 
-<h1>Reminder Marketing Site</h1>
-<Button />
-
-<p>
-	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<section>
+	<h1>
+		Remember which services you're paying for and when they're due for renewal
+	</h1>
+	<p>
+		It's easy to forget who you use for home insurance, car insurance and those
+		little extras that keep you going. Keep track of those and be told when
+		they're coming up for renewal.
+	</p>
+	<form>
+		<input type="text" placeholder="name@example.com" />
+		<input type="submit" value="Join waitlist" />
+	</form>
+</section>
