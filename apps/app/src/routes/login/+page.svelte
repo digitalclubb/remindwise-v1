73c8d70225b6c2aff223863c6480c20879306ef3
digitalclubb <thewelsh@gmail.com>
2023-06-03 18:44:35 +0100

    chore: basic login styles

--- apps/app/src/routes/login/+page.svelte
+++ apps/app/src/routes/login/+page.svelte
@@ -1,4 +1,3 @@
-<!-- src/routes/+page.svelte -->
 <script lang="ts">
 	import { Auth } from '@supabase/auth-ui-svelte';
 	import { ThemeSupa } from '@supabase/auth-ui-shared';
@@ -6,8 +5,9 @@
 	export let data;
 </script>
 
-<div class="row flex-center flex">
-	<div class="col-6 form-widget">
+<main>
+	<div>
+		<h1>Login</h1>
 		<Auth
 			additionalData="{undefined}"
 			supabaseClient="{data.supabase}"
@@ -15,6 +15,23 @@
 			redirectTo="{`${data.url}/logging-in?redirect=/`}"
 			showLinks="{true}"
 			appearance="{{ theme: ThemeSupa }}"
+			providers="{['google', 'facebook', 'twitter']}"
+			socialLayout="horizontal"
 		/>
 	</div>
-</div>
+</main>
+
+<style>
+	main {
+		display: grid;
+		place-items: center;
+		min-height: 100vh;
+	}
+
+	main > div {
+		width: 40rem;
+		border: solid 1px #d1d1d1;
+		border-radius: 0.2rem;
+		padding: 4rem;
+	}
+</style>
