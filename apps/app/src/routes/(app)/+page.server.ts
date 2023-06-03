6806e65e3377f09c331efdba7354afae6d6d5242
aferrugento <adrianaferrugento@gmail.com>
2023-06-03 12:03:40 +0100

    protect routes in layout

--- apps/app/src/routes/(app)/+page.server.ts
+++ apps/app/src/routes/(app)/+page.server.ts
@@ -3,11 +3,6 @@ import type { PageServerLoad } from './$types';
 
 export const load = (async ({ locals: { supabase, getSession } }) => {
 	const session = await getSession();
-
-	if (!session) {
-		throw redirect(303, '/login');
-	}
-
 	console.log('access token', session?.access_token);
 	console.log('access token', session?.user);
 	const { data: profile } = await supabase
