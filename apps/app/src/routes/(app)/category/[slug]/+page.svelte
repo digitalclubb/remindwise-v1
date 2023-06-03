e2e7ffe18a3130bb9f80111b67684485c95ff359
digitalclubb <thewelsh@gmail.com>
2023-06-03 21:56:49 +0100

    chore: reminder query

--- apps/app/src/routes/(app)/category/[slug]/+page.svelte
+++ apps/app/src/routes/(app)/category/[slug]/+page.svelte
@@ -7,6 +7,7 @@
 	$: category = $page.params.slug;
 
 	// TODO: Can we query reminders based on the response of the category Id
+	// https://stackoverflow.com/questions/47240085/pass-obtained-field-to-another-nested-query-in-graphql
 	$: categoryId = queryStore({
 		client,
 		query: gql`
@@ -24,6 +25,32 @@
 			category,
 		},
 	});
+
+	// TODO: unpause this once we have the Id back?
+	$: reminders = queryStore({
+		client,
+		query: gql`
+			query ($categoryId: String!) {
+				reminders: remindersCollection(
+					filter: { categoryId: { eq: $category } }
+				) {
+					list: edges {
+						reminder: node {
+							company
+							cost
+							dateOfRenewal
+							autoRenewal
+							reminderTypes
+						}
+					}
+				}
+			}
+		`,
+		variables: {
+			categoryId,
+		},
+		//pause: true,
+	});
 </script>
 
 <h1>{category}</h1>
@@ -37,5 +64,19 @@
 		{$categoryId.error.message}
 	{:else}
 		{$categoryId.data.categories.list[0].category.id}
+	{/if}
+</p>
+
+<p>
+	reminders:
+
+	{#if $reminders.fetching}
+		Loading...
+	{:else if $reminders.error}
+		{$reminders.error.message}
+	{:else}
+		{#each $reminders.data.reminders.list as reminder}
+			{reminder.reminder.company}
+		{/each}
 	{/if}
 </p>
