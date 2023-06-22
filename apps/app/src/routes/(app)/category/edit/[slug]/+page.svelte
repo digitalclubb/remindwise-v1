fb9f7136d6fe66f3a7d68f99207f6216ffea2af9
aferrugento <adrianaferrugento@gmail.com>
2023-06-22 11:40:19 +0100

    feat: add delete category functionality

---
+++ apps/app/src/routes/(app)/category/edit/[slug]/+page.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import Button from 'components/button/Button.svelte';
+	import Modal from '../../../../../components/modal/Modal.svelte';
+	import { goto } from '$app/navigation';
+	import { getContextClient, gql, mutationStore } from '@urql/svelte';
+
+	const client = getContextClient();
+
+	let showModal = false;
+
+	const onDelete = async () => {
+		mutationStore({
+			client,
+			query: gql`
+				mutation ($category: String!) {
+					deleteFromcategoriesCollection(filter: { name: { eq: $category } }) {
+						affectedCount
+					}
+				}
+			`,
+			variables: {
+				category: $page.params.slug,
+			},
+		}).subscribe((result) => {
+			if (result.error) {
+				// Error
+				console.log('Error', result);
+			}
+
+			if (result.data) {
+				goto('/');
+			}
+		});
+	};
+</script>
+
+<h1>Edit <span>{$page.params.slug}</span> category</h1>
+
+<Button on:click="{() => (showModal = true)}">Delete Category</Button>
+
+<Modal bind:showModal="{showModal}">
+	<h2>Are you sure you want to delete this category?</h2>
+	<p>
+		Deleting this category will delete all of the reminders associated with it.
+	</p>
+
+	<Button on:click="{onDelete}">Delete</Button>
+</Modal>
