e087f4e758e26bae6e70afdf3506b17de110dc06
Gareth Clubb <thewelsh@gmail.com>
2023-07-11 12:13:54 +0100

    chore: remove baseUrl

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
 	workers: process.env.CI ? 1 : undefined,
 	reporter: 'html',
 	use: {
-		baseURL: 'http://localhost:4173',
 		trace: 'on-first-retry',
 	},
 	projects: [
@@ -23,7 +22,6 @@ export default defineConfig({
 	],
 	webServer: {
 		command: 'pnpm preview',
-		url: 'http://localhost:4173',
 		reuseExistingServer: !process.env.CI,
 	},
 });
