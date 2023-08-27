73f2f281453c0dabc9c15b17b4b7730cf51952b5
aferrugento <adrianaferrugento@gmail.com>
2023-08-27 16:07:32 +0100

    remove var

--- apps/app/src/client.ts
+++ apps/app/src/client.ts
@@ -1,8 +1,8 @@
 import { HoudiniClient } from '$houdini';
-import { PUBLIC_SUPABASE_GRAPHQL_URL } from '$env/static/public';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY } from '$env/static/public';
 
 export default new HoudiniClient({
-	url: PUBLIC_SUPABASE_GRAPHQL_URL,
+	url: `${PUBLIC_SUPABASE_URL}/graphql/v1?apikey=${PUBLIC_SUPABASE_KEY}`,
 	fetchParams({ session }) {
 		return {
 			headers: {
