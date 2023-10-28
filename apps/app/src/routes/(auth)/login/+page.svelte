b4e148f60b72123289dfc9f28b666883fb5730c2
Gareth Clubb <thewelsh@gmail.com>
2023-10-28 11:14:50 +0100

    chore: styles

--- apps/app/src/routes/(auth)/login/+page.svelte
+++ apps/app/src/routes/(auth)/login/+page.svelte
@@ -10,41 +10,69 @@
 	<title>remindwise.io - Login</title>
 </svelte:head>
 
-<div class="box">
-	<div class="box-manual">
+<div class="boxes">
+	<div class="box box-manual">
 		<h1 class="heading-3">Login to your account</h1>
 		<form method="post" use:enhance>
-			<label for="email">Email Address</label>
-			<input id="email" name="email" type="email" value={form?.email ?? ''} />
+			<label for="email">Email</label>
+			<input
+				id="email"
+				name="email"
+				type="email"
+				value={form?.email ?? ''}
+				placeholder="Enter your email address"
+			/>
 			<label for="password">Password</label>
 			<input
 				id="password"
 				name="password"
 				type="password"
 				autocomplete="current-password"
+				placeholder="Enter your password"
 			/>
 
-			<Button>Sign in</Button>
+			<a href="" class="forgotten">Forgotten your password?</a>
+			<Button>Login</Button>
 		</form>
 	</div>
-	<div class="box-social">
+	<div class="box box-social">
 		<h2 class="heading-5">Or</h2>
+		<ul>
+			<li>
+				<a href="" class="social-login">
+					<img src="/icon-facebook.svg" alt="" /> Login with Facebook
+				</a>
+			</li>
+			<li>
+				<a href="" class="social-login"
+					><img src="/icon-google.svg" alt="" /> Login with Google
+				</a>
+			</li>
+			<li>
+				<a href="" class="social-login"
+					><img src="/icon-x.svg" alt="" /> Login with X</a
+				>
+			</li>
+		</ul>
 	</div>
 </div>
 
 <p>
-	<span>Don't have a remindwise account yet?</span>
+	<span class="register-heading">Don't have a remindwise account yet?</span>
 	<Link href="/register" type="button">Register now</Link>
 </p>
 
 <style>
-	.box {
+	.boxes {
 		background-color: var(--white);
 		border-radius: 0.6rem;
 		margin-bottom: 3rem;
+		display: flex;
+		flex-wrap: wrap;
+		flex-direction: column;
 	}
 
-	.box-manual {
+	.box {
 		padding: 1.9rem 2.6rem 3.2rem 2.3rem;
 	}
 
@@ -52,7 +80,6 @@
 		border-top: solid 1px var(--cream);
 		background-color: var(--cream-light);
 		text-align: center;
-		padding: 2.5rem 3rem 4.7rem 3rem;
 	}
 	form {
 		display: flex;
@@ -75,5 +102,53 @@
 	span {
 		display: block;
 		margin-bottom: 2rem;
+	}
+
+	input {
+		display: block;
+		border: 1px solid var(--grey-light);
+		border-radius: 0.6rem;
+		padding: 0.9rem 1.4rem;
+	}
+
+	.forgotten {
+		display: block;
+		margin-top: 2rem;
+		margin-bottom: 1.8rem;
+		color: var(--orange);
+		text-decoration: none;
+	}
+
+	.forgotten:hover {
+		text-decoration: underline;
+	}
+
+	li {
+		margin-top: 2rem;
+	}
+
+	.social-login {
+		display: flex;
+		align-items: center;
+		background: linear-gradient(180deg, #fff 0%, #f6f6f6 100%);
+		color: var(--grey);
+		text-decoration: none;
+		border: 1px solid var(--grey-light);
+		border-radius: 2rem;
+		text-align: left;
+		padding: 0.9rem 1.4rem;
+		width: 21.4rem;
+	}
+
+	.social-login img {
+		height: 1rem;
+		margin-right: 1rem;
+	}
+
+	.social-login:hover {
+		background: linear-gradient(180deg, #f6f6f6 0%, var(--grey-light) 100%);
+	}
+
+	@media screen and (min-width: 768px) {
 	}
 </style>
