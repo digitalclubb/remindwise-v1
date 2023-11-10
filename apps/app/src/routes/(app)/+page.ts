e500d38694583f712443100b073764da1289575c
aferrugento <adrianaferrugento@gmail.com>
2023-11-10 10:14:17 +0000

    feat: add filters to upcoming tables (#142)

--- apps/app/src/routes/(app)/+page.ts
+++ apps/app/src/routes/(app)/+page.ts
@@ -4,7 +4,7 @@ import { load_getAllReminders } from '$houdini';
 export const load: PageLoad = async (event) => {
 	const today = new Date();
 	const upcoming = new Date();
-	upcoming.setMonth(upcoming.getMonth() + 2);
+	upcoming.setMonth(upcoming.getMonth() + 1);
 
 	return {
 		...(await load_getAllReminders({
