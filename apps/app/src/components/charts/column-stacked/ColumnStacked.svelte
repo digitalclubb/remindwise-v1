143fbb6b2bf6070297c7344a1a791a2ceb8e2d92
Gareth Clubb <thewelsh@gmail.com>
2023-11-13 20:26:27 +0000

    chore: rounded

--- apps/app/src/components/charts/column-stacked/ColumnStacked.svelte
+++ apps/app/src/components/charts/column-stacked/ColumnStacked.svelte
@@ -2,15 +2,18 @@
 	import { getContext } from 'svelte';
 
 	const { data, xGet, yGet, zGet, xScale } = getContext('LayerCake');
+
+	const isRounded = (color) => (color === 'var(--orange)');
 </script>
 
 <g class="column-group">
-	{#each $data as series}
+	{#each [...$data].reverse() as series}
 		{#each series as d}
 			{@const yVals = $yGet(d)}
 			{@const columnHeight = yVals[0] - yVals[1]}
 
-			<path stroke="{$zGet(series)}" stroke-width="8" stroke-linejoin="round" d="M{$xGet(d)},{yVals[1]} h{$xScale.bandwidth()} v{columnHeight} H{$xGet(d)}Z" fill={$zGet(series)} />
+			<path stroke="{$zGet(series)}" stroke-width="8" stroke-linejoin={isRounded($zGet(series)) ? 'round' : undefined} d="M{$xGet(d)},{yVals[1]} h{$xScale.bandwidth()} v{columnHeight} H{$xGet(d)}Z" fill={$zGet(series)} />
 		{/each}
 	{/each}
 </g>
+
