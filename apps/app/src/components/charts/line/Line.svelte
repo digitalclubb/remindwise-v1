2a64fc13db2b0f8e1bffbf34a238f7e625d308c9
Gareth Clubb <thewelsh@gmail.com>
2023-11-18 21:04:53 +0000

    upcoming line

--- apps/app/src/components/charts/line/Line.svelte
+++ apps/app/src/components/charts/line/Line.svelte
@@ -4,19 +4,32 @@
 
 	const { data, xGet, yGet } = getContext<LayerCakeContext>('LayerCake');
 
-	export let stroke = 'var(--grey-dark)';
+	const month = new Date().getMonth();
 
 	$: path =
+		'M' +
+		$data
+			.slice(0, month + 1)
+			.map((d: unknown) => $xGet(d) + ',' + $yGet(d))
+			.join('L');
+
+	$: upcoming =
 		'M' + $data.map((d: unknown) => $xGet(d) + ',' + $yGet(d)).join('L');
 </script>
 
-<path class="path-line" d={path} {stroke}></path>
+<path class="path-line upcoming" d={upcoming}></path>
+<path class="path-line" d={path}></path>
 
 <style>
 	.path-line {
 		fill: none;
+		stroke: var(--grey-dark);
 		stroke-linejoin: round;
 		stroke-linecap: round;
 		stroke-width: 3;
+	}
+
+	.upcoming {
+		stroke: var(--orange);
 	}
 </style>
