afae717df101bf66650b59c2b97610bdb824b131
Gareth Clubb <thewelsh@gmail.com>
2023-12-17 07:57:59 +0000

    chore: padding

--- apps/app/src/components/header/Header.svelte
+++ apps/app/src/components/header/Header.svelte
@@ -29,7 +29,7 @@
 <style>
 	header {
 		background: linear-gradient(180deg, #fff 0%, #f6f6f6 100%);
-		padding: 2rem 4.2rem;
+		padding: 2rem 2.1rem;
 		display: flex;
 		justify-content: space-between;
 	}
@@ -84,5 +84,12 @@
 		display: flex;
 		justify-content: center;
 		align-items: center;
+	}
+
+	@media screen and (min-width: 1024px) {
+		header {
+			padding-left: 4.2rem;
+			padding-right: 4.2rem;
+		}
 	}
 </style>
