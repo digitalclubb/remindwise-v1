fb81e4d2698a69dfc247ec99d305316edd516545
Adriana Ferrugento <adriana.ferrugento@zoopla.co.uk>
2024-06-10 10:51:43 +0100

    less flaky

--- apps/app/tests/auth.setup.ts
+++ apps/app/tests/auth.setup.ts
@@ -5,7 +5,10 @@ const authFile = 'playwright/.auth/user.json';
 const { PLAYWRIGHT_USERNAME, PLAYWRIGHT_PASSWORD } = process.env;
 
 setup('authenticate', async ({ page }) => {
-	await page.goto('/login');
+	await page.goto('/login', {waitUntil: 'domcontentloaded'});
+
+	await page.getByText('Login to your account').waitFor();
+	
 	await page.getByLabel('Email').fill(PLAYWRIGHT_USERNAME as string);
 	await page.getByLabel('Password').fill(PLAYWRIGHT_PASSWORD as string);
 	await page.getByRole('button', { name: 'Login' }).click();
