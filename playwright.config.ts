235d4777d309ad19f0377b7d4317060ec6ec4f42
Gareth Clubb <thewelsh@gmail.com>
2024-06-10 09:52:58 +0100

    chore: disable reporting

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
 	retries: process.env.CI ? 2 : 0,
 	workers: process.env.CI ? 1 : undefined,
 	timeout: 60000,
-	reporter: 'html',
+	reporter: [['html', { open: 'never' }]],
 	use: {
 		trace: 'on-first-retry',
 	},
