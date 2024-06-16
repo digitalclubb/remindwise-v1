1f930f02b890166ed6839ae92ee8c3ba5efe3054
Adriana Ferrugento <adrianaferrugento@gmail.com>
2024-06-16 09:23:13 +0100

    test

--- apps/app/src/routes/(auth)/forgot-password/+page.server.ts
+++ apps/app/src/routes/(auth)/forgot-password/+page.server.ts
@@ -19,6 +19,7 @@ export const actions: Actions = {
 			return fail(400, { form });
 		}
 
+		console.log('url', url.origin);
 		const { error } = await supabase.auth.resetPasswordForEmail(data.email, {
 			redirectTo: `${url.origin}/auth/callback?goto=/update-password`,
 		});
