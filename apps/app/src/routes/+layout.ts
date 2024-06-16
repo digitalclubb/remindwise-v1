b16828b257f64c47905f13e1ad1cc127e1632e74
Gareth Clubb <thewelsh@gmail.com>
2024-06-16 09:20:46 +0100

    chore: browser env

--- apps/app/src/routes/+layout.ts
+++ apps/app/src/routes/+layout.ts
@@ -1,7 +1,7 @@
-import { dev } from '$app/environment';
+import { browser, dev } from '$app/environment';
 import { init, trackEvent } from '@aptabase/web';
 
-if (!dev) {
+if (!dev && browser) {
 	init('A-EU-1548251116', { appVersion: '1' });
 	trackEvent('app_load');
 }
--- apps/marketing/src/routes/+layout.ts
+++ apps/marketing/src/routes/+layout.ts
@@ -1,7 +1,7 @@
-import { dev } from '$app/environment';
+import { browser, dev } from '$app/environment';
 import { init, trackEvent } from '@aptabase/web';
 
-if (!dev) {
+if (!dev && browser) {
 	init('A-EU-8881808042', { appVersion: '1' });
 	trackEvent('app_load');
 }
