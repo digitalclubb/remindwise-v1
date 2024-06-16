48b45ab74d336bf2acd05513eaa4c095a8e95714
Adriana Ferrugento <adrianaferrugento@gmail.com>
2024-06-16 10:34:14 +0100

    log vars

--- apps/app/src/routes/(auth)/util.ts
+++ apps/app/src/routes/(auth)/util.ts
@@ -1,4 +1,12 @@
 export const getURL = () => {
+	console.log(
+		'process.env.NEXT_PUBLIC_SITE_URL',
+		process?.env?.NEXT_PUBLIC_SITE_URL
+	);
+	console.log(
+		'process.env.NEXT_PUBLIC_VERCEL_URL',
+		process?.env?.NEXT_PUBLIC_VERCEL_URL
+	);
 	let url =
 		process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
 		process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
