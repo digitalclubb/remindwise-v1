b213e107c5971d9ad057d56cf492c97f7ab9e3fe
Adriana Ferrugento <adrianaferrugento@gmail.com>
2024-06-17 10:04:49 +0100

    try

--- apps/app/src/routes/(auth)/util.ts
+++ apps/app/src/routes/(auth)/util.ts
@@ -1,15 +1,12 @@
 export const getURL = () => {
+	console.log('process.env.VITE_VERCEL_URL', process?.env?.VITE_VERCEL_URL);
 	console.log(
-		'process.env.NEXT_PUBLIC_SITE_URL',
-		process?.env?.NEXT_PUBLIC_SITE_URL
-	);
-	console.log(
-		'process.env.NEXT_PUBLIC_VERCEL_URL',
-		process?.env?.NEXT_PUBLIC_VERCEL_URL
+		'process.env.VITE_VERCEL_PROJECT_PRODUCTION_URL',
+		process?.env?.VITE_VERCEL_PROJECT_PRODUCTION_URL
 	);
 	let url =
-		process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-		process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+		process?.env?.VITE_VERCEL_PROJECT_PRODUCTION_URL ?? // Set this to your site URL in production env.
+		process?.env?.VITE_VERCEL_URL ?? // Automatically set by Vercel.
 		'http://localhost:3000/';
 	// Make sure to include `https://` when not localhost.
 	url = url.startsWith('http') ? url : `https://${url}`;
