c8b790885f314782dde6f33896320e880ec206d5
Adriana Ferrugento <adrianaferrugento@gmail.com>
2024-06-17 11:13:23 +0100

    fix url

--- apps/app/src/routes/(auth)/util.ts
+++ apps/app/src/routes/(auth)/util.ts
@@ -1,16 +1,17 @@
 export const getURL = () => {
-	console.log('process.env.VERCEL_URL', process?.env?.VERCEL_URL);
-	console.log(
-		'process.env.VERCEL_PROJECT_PRODUCTION_URL',
-		process?.env?.VERCEL_PROJECT_PRODUCTION_URL
-	);
-	let url =
-		process?.env?.VERCEL_PROJECT_PRODUCTION_URL ?? // Set this to your site URL in production env.
-		process?.env?.VERCEL_URL ?? // Automatically set by Vercel.
-		'http://localhost:3000/';
-	// Make sure to include `https://` when not localhost.
+	let url = '';
+	switch (process?.env?.VERCEL_ENV) {
+		case 'production':
+			url = process?.env?.VERCEL_PROJECT_PRODUCTION_URL ?? '';
+			break;
+		case 'preview':
+			url = process?.env?.VERCEL_URL ?? '';
+			break;
+		default:
+			url = 'http://localhost:3000/';
+	}
+
 	url = url.startsWith('http') ? url : `https://${url}`;
-	// Make sure to include a trailing `/`.
 	url = url.endsWith('/') ? url : `${url}/`;
 	return url;
 };
