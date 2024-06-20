09fd9304b175495b66270cbe7b4139b31227ed84
Gareth Clubb <thewelsh@gmail.com>
2024-06-20 21:25:23 +0100

    chore: getAllReminders

--- apps/app/src/mocks/handlers/getAllReminders.ts
+++ apps/app/src/mocks/handlers/getAllReminders.ts
@@ -1,12 +1,37 @@
 import { graphql, HttpResponse } from 'msw';
 import type { GetAllRemindersQuery } from '@graphql/types';
 
+const reminder = {
+	id: 1,
+	name: 'Upcoming reminder',
+	company: 'Example company',
+	cost: 10,
+	total: 10,
+	date: '2024-10-10',
+	autoRenewal: false,
+	category: {
+		iconId: 'watch',
+		name: 'Example',
+	},
+};
+
 export const getAllReminders = graphql.query<GetAllRemindersQuery>(
 	'getAllReminders',
 	() => {
 		return HttpResponse.json(
 			{
-				data: {},
+				data: {
+					upcoming: {
+						list: [{ reminder }],
+					},
+					reminders: {
+						pageInfo: {
+							hasPreviousPage: false,
+							hasNextPage: true,
+						},
+						list: [{ reminder }],
+					},
+				},
 			},
 			{
 				headers: {
