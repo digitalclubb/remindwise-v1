e9a89644c2ea7b4d12e4c5fb25d9d551f9dc56e1
Gareth Clubb <thewelsh@gmail.com>
2024-06-20 22:01:25 +0100

    chore: removing getAllRemidners

--- apps/app/src/mocks/handlers.ts
+++ apps/app/src/mocks/handlers.ts
@@ -1,13 +1,6 @@
-import { getAllReminders } from './handlers/getAllReminders';
 import { getCategories } from './handlers/getCategories';
 import { getReminder } from './handlers/getReminder';
 import { getReminders } from './handlers/getReminders';
 import { getSettings } from './handlers/getSettings';
 
-export const handlers = [
-	getAllReminders,
-	getCategories,
-	getReminder,
-	getReminders,
-	getSettings,
-];
+export const handlers = [getCategories, getReminder, getReminders, getSettings];
