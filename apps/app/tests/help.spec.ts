8e48902dcece2f3236885de45bdba8216f8f5117
Gareth Clubb <thewelsh@gmail.com>
2024-06-21 21:13:28 +0100

    chore: visual app

--- apps/app/tests/help.spec.ts
+++ apps/app/tests/help.spec.ts
@@ -29,4 +29,16 @@ test.describe('Help page', () => {
 			expect(results.violations).toEqual([]);
 		}
 	);
+
+	test(
+		'has visual test',
+		{
+			tag: '@visual',
+		},
+		async ({ page }) => {
+			await expect(page).toHaveScreenshot({
+				fullPage: true,
+			});
+		}
+	);
 });
