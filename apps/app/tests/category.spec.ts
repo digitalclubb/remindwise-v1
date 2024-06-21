377a8de3ecae62a42fcfd6fea3b9018f5a409721
Adriana Ferrugento <adrianaferrugento@gmail.com>
2024-06-21 14:41:12 +0100

    pom

---
+++ apps/app/tests/category.spec.ts
@@ -0,0 +1,32 @@
+import { test, expect } from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright';
+import { Category } from './pom/category';
+
+let category: Category;
+test.describe('Category page', () => {
+	test.beforeEach(async ({ page }) => {
+		category = new Category(page);
+		await category.goto();
+	});
+
+	test(
+		'has correct title',
+		{
+			tag: '@functional',
+		},
+		async () => {
+			await expect(category.pageTitle).toHaveText('Example 1');
+		}
+	);
+
+	test(
+		'has no accessibility violations',
+		{
+			tag: '@accessibility',
+		},
+		async ({ page }) => {
+			const results = await new AxeBuilder({ page }).analyze();
+			expect(results.violations).toEqual([]);
+		}
+	);
+});
---
+++ apps/app/tests/settings.spec.ts
@@ -0,0 +1,32 @@
+import { test, expect } from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright';
+import { Settings } from './pom/settings';
+
+let settings: Settings;
+test.describe('Settings page', () => {
+	test.beforeEach(async ({ page }) => {
+		settings = new Settings(page);
+		await settings.goto();
+	});
+
+	test(
+		'has correct title',
+		{
+			tag: '@functional',
+		},
+		async () => {
+			await expect(settings.pageTitle).toHaveText('Example 1');
+		}
+	);
+
+	test(
+		'has no accessibility violations',
+		{
+			tag: '@accessibility',
+		},
+		async ({ page }) => {
+			const results = await new AxeBuilder({ page }).analyze();
+			expect(results.violations).toEqual([]);
+		}
+	);
+});
