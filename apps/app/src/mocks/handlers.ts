c268330a4cd1a561fdda1c35d76ec53b39d20c67
Gareth Clubb <thewelsh@gmail.com>
2024-06-22 21:09:35 +0100

    chore: updateSettings

--- apps/app/src/mocks/handlers.ts
+++ apps/app/src/mocks/handlers.ts
@@ -1,6 +1,10 @@
+// Queries
 import { getCategories } from './handlers/getCategories';
 import { getReminder } from './handlers/getReminder';
 import { getReminders } from './handlers/getReminders';
 import { getSettings } from './handlers/getSettings';
 
-export const handlers = [getCategories, getReminder, getReminders, getSettings];
+// Mutations
+import { updateSettings } from './handlers/updateSettings';
+
+export const handlers = [getCategories, getReminder, getReminders, getSettings, updateSettings];
