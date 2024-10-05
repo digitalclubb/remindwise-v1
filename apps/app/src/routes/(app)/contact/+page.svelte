bc404d3b4c77cf115285be040fc446c49852f58d
Gareth Clubb <thewelsh@gmail.com>
2024-10-05 20:19:54 +0100

    feat: contact page (#368)

---
+++ apps/app/src/routes/(app)/contact/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import Header from '../../../components/header/Header.svelte';
+</script>
+
+<svelte:head>
+	<title>Get in touch · remindwise.io</title>
+</svelte:head>
+
+<Header title="Get in touch" />
+
+<article class="body">
+	<h2 class="heading-3">Email</h2>
+	<p>
+		For any enquiries, please reach out to us on <a
+			href="mailto:help@remindwise.io">help@remindwise.io</a>
+	</p>
+</article>
+
+<style>
+	a {
+		color: var(--orange);
+	}
+
+	a:hover {
+		text-decoration: none;
+	}
+</style>
--- apps/app/src/routes/(app)/notifications/+page.svelte
+++ apps/app/src/routes/(app)/notifications/+page.svelte
@@ -7,3 +7,12 @@
 </svelte:head>
 
 <Header title="Notifications" />
+
+<article class="body">
+	<h2 class="heading-3">Coming soon</h2>
+	<p>We want to ensure you never miss an important reminder!</p>
+	<p>
+		That's why we're excited to let you know that we'll soon be adding a list of
+		active reminders that are due right here. Stay tuned!
+	</p>
+</article>
